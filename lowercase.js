cc91db7d1b83ea0ad1e4c0e1bd1f6ed87d03cc65
Jakub Hampl <honitom@seznam.cz>
2011-03-17 10:40:09 +0000

    Added ability to toggle cases when selected.

--- lowercase.js
+++ lowercase.js
@@ -1,15 +1,48 @@
+/* To Title Case 1.1.1
+ * David Gouch <http://individed.com>
+ * 23 May 2008
+ * License: http://individed.com/code/to-title-case/license.txt
+ *
+ * In response to John Gruber's call for a Javascript version of his script: 
+ * http://daringfireball.net/2008/05/title_case
+ */
+
+String.prototype.toTitleCase = function() {
+    return this.replace(/([\w&`'‘’"“.@:\/\{\(\[<>_]+-? *)/g, function(match, p1, index, title) {
+        if (index > 0 && title.charAt(index - 2) !== ":" &&
+        	match.search(/^(a(nd?|s|t)?|b(ut|y)|en|for|i[fn]|o[fnr]|t(he|o)|vs?\.?|via)[ \-]/i) > -1)
+            return match.toLowerCase();
+        if (title.substring(index - 1, index + 1).search(/['"_{(\[]/) > -1)
+            return match.charAt(0) + match.charAt(1).toUpperCase() + match.substr(2);
+        if (match.substr(1).search(/[A-Z]+|&|[\w]+[._][\w]+/) > -1 || 
+        	title.substring(index - 1, index + 1).search(/[\])}]/) > -1)
+            return match;
+        return match.charAt(0).toUpperCase() + match.substr(1);
+    });
+};
+
 add_button({
   title: "Convert to lowercase",
   id: 'lowercase',
   name: '&darr;',
   pos: 100,
   callback: function() {
-    actOnSelection(function(txt, isSelection) { return clean(txt, function(line) {
+    actOnSelection(function(txt, isSelection) { 
       if(isSelection) {
-        return line.toLowerCase();
+        if(txt.toLowerCase() == txt) {
+          return txt.toTitleCase();
+        } else if(txt.toTitleCase() == txt) {
+          return txt.toUpperCase();
+        } else {
+          return txt.toLowerCase();
+        }
       } else {
-        return line.replace(/([A-Z])([A-Z]+)/g, function() {return arguments[1] + arguments[2].toLowerCase()});
-      }
-    }); }, false);
+        return clean(txt, function(line) {
+          return line.replace(/([A-Z])([A-Z]+)/g, function() {
+            return arguments[1] + arguments[2].toLowerCase();
+          });
+        }); 
+      }  
+    }, false);
   }
 });
